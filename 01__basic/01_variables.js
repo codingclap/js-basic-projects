b43fc5107b654142f7d547708b00c10a8f4fa30a
Anuj Kumar <86232374+codingclap@users.noreply.github.com>
2024-02-09 13:21:11 +0000

    varable part is done now

---
+++ 01__basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 23222;                               // can't be overwrite it's value
+let userName = "amit kumar";                            
+var accountName = "232323"; 
+mapId = "this is not a Variable";                       // Not to decraled variable like this 
+let accountState;                       
+
+userName = "updated name";                   // Not to decraled variable like this  
+accountName = "jijoooo";                     // Undefined Variable
+
+/*
+Prefer not to use var because it has block and functional scope
+*/
+
+
+console.table([accountId, userName, accountName, mapId, accountState])
